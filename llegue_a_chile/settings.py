8cd5113ebdc4287c935bdd033c2ac598cd08360a
Catherine Basaez <cat.basaez@alumnos.duoc.cl>
2018-11-07 11:03:19 -0300

    ya se ve !!! Y se integro postgres

--- llegue_a_chile/settings.py
+++ llegue_a_chile/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'llegue_a_chile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dfujmxsr',
+        'USER': 'dfujmxsr',
+        'PASSWORD': 'Vanfh7A2WOfbJU9sofQTHWwXehH4H0ob',
+        'HOST': 'elmer.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
@@ -119,4 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
