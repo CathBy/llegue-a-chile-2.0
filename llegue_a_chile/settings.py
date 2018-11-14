232995b23be973a1aadeeb9991a59e28a72c21de
Catherine Basaez <cat.basaez@alumnos.duoc.cl>
2018-11-13 22:28:59 -0300

    cambio database settings

--- llegue_a_chile/settings.py
+++ llegue_a_chile/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'llegue_a_chile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dfujmxsr',
-        'USER': 'dfujmxsr',
-        'PASSWORD': 'Vanfh7A2WOfbJU9sofQTHWwXehH4H0ob',
-        'HOST': 'elmer.db.elephantsql.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
